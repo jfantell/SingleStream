cc9b5947c00a23d70a858aa2b64deb326bc8e335
John Fantell <johnfantell@squat-thrust-110.dynamic2.rpi.edu>
2017-04-22 16:15:24 -0400

    Adding Flash fix?

--- single_stream_app/views/playlist_resources/napster_client.js
+++ single_stream_app/views/playlist_resources/napster_client.js
@@ -1,18 +1,41 @@
-function player(){
+// var player_set = false;
+
+function player(id,operation){
   var API_KEY = 'YWQzNWQxZWMtNDU5Mi00NjJjLThkNTAtNjcwN2M4Yjc5NWM4';
   $.get("/tracks_info", function(data, status){
     Napster.init({ consumerKey: API_KEY });
 
-    var currentTrack;
+    // if(!player_set){
     Napster.player.on('ready', function(e) {
-
       Napster.member.set({
-          accessToken: data.access_token,
-          refreshToken: data.refresh_token
+        accessToken: data.access_token,
+        refreshToken: data.refresh_token
       });
+      console.log("You reached here as well");
+      
+      if(operation == 'play'){
+        Napster.player.play(id);
+        console.log("Play");
+      }
+      else if(operation == 'pause'){
+        Napster.player.pause();
+        console.log("Pause");
+      }
+      player_set= true;
       console.log(data.access_token, data.refresh_token);
-
     });
+    // }
+    
+    // if(player_set){
+    //   if(operation == 'play'){
+    //     Napster.player.play(id);
+    //     console.log("Play");
+    //   }
+    //   else if(operation == 'pause'){
+    //     Napster.player.pause();
+    //     console.log("Pause");
+    //   }
+    // }
 
     Napster.player.on('error', console.log);
   });
